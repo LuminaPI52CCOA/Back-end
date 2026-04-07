c373b5255e0b2f1542338951603d0aa8cb4e4297
LuizSouzaSP <luizh.souza@sptech.school>
2026-04-07 16:42:53 -0300

    add(EntidadeNaoEncontrada): Excessao criada

---
+++ src/main/java/com/lumina/backend/exception/EntidadeNaoEncontrada.java
@@ -0,0 +1,7 @@
+package com.lumina.backend.exception;
+
+public class EntidadeNaoEncontrada extends RuntimeException {
+    public EntidadeNaoEncontrada(String message) {
+        super(message);
+    }
+}
