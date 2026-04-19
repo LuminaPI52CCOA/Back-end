f41302e9c5e0d3cd4c7ed0d566b8f675ed2b7d10
LuizSouzaSP <luizh.souza@sptech.school>
2026-04-19 19:53:31 -0300

    add(ClienteController): endpoints de listagem de anamnese por cliente e cadastro da anamnese para o cliente

--- src/main/java/com/lumina/backend/controller/ClienteController.java
+++ src/main/java/com/lumina/backend/controller/ClienteController.java
@@ -1,8 +1,12 @@
 package com.lumina.backend.controller;
 
+import com.lumina.backend.dto.anamnese.AnamneseMapper;
+import com.lumina.backend.dto.anamnese.AnamneseRequest;
+import com.lumina.backend.dto.anamnese.AnamneseResponse;
 import com.lumina.backend.dto.cliente.ClienteMapper;
 import com.lumina.backend.dto.cliente.ClienteRequest;
 import com.lumina.backend.dto.cliente.ClienteResponse;
+import com.lumina.backend.model.Anamnese;
 import com.lumina.backend.model.Cliente;
 import com.lumina.backend.service.cliente.ClienteService;
 import jakarta.validation.Valid;
@@ -53,4 +57,21 @@ public class ClienteController {
         ClienteResponse response = ClienteMapper.toDto(cliente);
         return ResponseEntity.ok(response);
     }
+
+    @GetMapping("/{id}/anamneses")
+    public ResponseEntity<List<AnamneseResponse>> listarAnamnese(@PathVariable Integer id){
+        List<Anamnese> anamneseList = service.listarAnamnese(id);
+        List<AnamneseResponse> response = AnamneseMapper.toDto(anamneseList);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/{id}/anamneses")
+    public ResponseEntity<AnamneseResponse> cadastroAnamnese(@PathVariable Integer id,
+                                                             @RequestBody AnamneseRequest request){
+        Anamnese anamnese = service.cadastrarAnamnese(id, request);
+        AnamneseResponse response = AnamneseMapper.toDto(anamnese);
+        return ResponseEntity.ok(response);
+    }
+
+
 }
