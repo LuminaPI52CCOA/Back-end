4feb8effca23e6b4b2fe24e25ea53bc6160485ac
CaioSousavs <joao.sousa@sptech.school>
2026-05-31 04:22:46 -0300

    Feat(AnamneseService): Criando classe para processar e inserir conteudo da OCR dentro do Banco de Dados.

--- src/main/java/com/lumina/backend/service/anamnese/AnamneseService.java
+++ src/main/java/com/lumina/backend/service/anamnese/AnamneseService.java
@@ -34,7 +34,98 @@ public class AnamneseService {
 
     public AnamneseService(AnamneseRepository repository, TextractClient textractClient, GeminiAIService geminiAIService,  AnamneseMapper anamneseMapper) {
         this.repository = repository;
+        this.textractClient = textractClient;
+        this.geminiAIService = geminiAIService;
+        this.anamneseMapper = anamneseMapper;
     }
+
+
+
+    public Anamnese processImage(
+            MultipartFile file
+    ) throws IOException {
+
+        log.trace("Processando imagem da anamnese no OCR");
+
+        if(file.isEmpty()){
+            throw new AnamneseVazio("Arquivo não encontrado.");
+        }
+
+        byte[] bytes = file.getBytes();
+
+        DetectDocumentTextRequest request =
+                DetectDocumentTextRequest.builder()
+                        .document(
+                                Document.builder()
+                                        .bytes(
+                                                SdkBytes.fromByteArray(bytes)
+                                        )
+                                        .build()
+                        )
+                        .build();
+
+        DetectDocumentTextResponse response =
+                textractClient.detectDocumentText(
+                        request
+                );
+
+        String ocr =
+                response.blocks()
+                        .stream()
+                        .filter(
+                                block ->
+                                        block.blockType() == BlockType.LINE
+                        )
+                        .map(
+                                block -> block.text()
+                        )
+                        .collect(
+                                Collectors.joining("\n")
+                        );
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        log.trace("Transformando as informações da OCR em Json");
+        String retornoApi = geminiAIService.transformarEmJson(ocr);
+        log.trace("Limpando os dados divergentes");
+        JsonNode json = geminiAIService.limparJsonGemini(retornoApi);
+        log.trace("Realizando insert no banco de dados");
+
+            OcrRespostaDTO.FichaAnamneseWrapperDTO ficha = mapper.treeToValue(json, OcrRespostaDTO.FichaAnamneseWrapperDTO.class);
+            OcrRespostaDTO.PerguntasAnamneseDTO perguntas = ficha.fichaAnamnese().perguntas();
+
+
+        Anamnese anamnese = new Anamnese();
+
+        anamnese.setDataAnamnese(LocalDate.now());
+        anamnese.setFazendoTratamento(perguntas.q1().respostaSim());
+        anamnese.setDescricaoTratamento(perguntas.q1().detalhes());
+        anamnese.setDoresCabecaFaceAtm(perguntas.q2().respostaSim());
+        anamnese.setAlergiaMedicamentos(perguntas.q3().respostaSim());
+        anamnese.setDescricaoAlergiaMedicamentos(perguntas.q3().detalhes());
+        anamnese.setReacaoAnestesiaLocal(perguntas.q4().respostaSim());
+        anamnese.setSensibilidadeDentaria(perguntas.q5().respostaSim());
+        anamnese.setBruxismoApertamento(perguntas.q6().respostaSim());
+        anamnese.setSangramentoGengival(perguntas.q7().respostaSim());
+        anamnese.setPossuiHabito(perguntas.q8().respostaSim());
+        anamnese.setDescricaoHabito(perguntas.q8().detalhes());
+        anamnese.setHistoricoDiabetes(perguntas.q9().respostaSim());
+        anamnese.setSangramentoExcessivo(perguntas.q10().respostaSim());
+        anamnese.setProblemaCardiaco(perguntas.q11().respostaSim());
+        anamnese.setDescricaoProblemaCardiaco(perguntas.q11().detalhes());
+        anamnese.setPressaoArterialNormal(perguntas.q12().respostaSim());
+        anamnese.setDescricaoPressaoArterial(perguntas.q12().detalhes());
+        anamnese.setHistoricoDiabetes(perguntas.q13().respostaSim());
+        anamnese.setGestante(perguntas.q14().respostaSim());
+
+        Cliente cliente = new Cliente();
+        cliente.setIdCliente(1L);
+
+            anamnese.setFkCliente(cliente);
+
+        return repository.save(anamnese);
+    }
+
 
     public Anamnese buscarPorId(Integer id){
         return repository.findById(id)
