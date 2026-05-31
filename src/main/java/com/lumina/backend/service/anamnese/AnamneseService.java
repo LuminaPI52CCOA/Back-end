1dea0211a28499e5a3791860387dac99aa944ea0
CaioSousavs <joao.sousa@sptech.school>
2026-05-31 04:11:35 -0300

    Feat(AnamneseService): Adicionando os imports necessarios para o código.

--- src/main/java/com/lumina/backend/service/anamnese/AnamneseService.java
+++ src/main/java/com/lumina/backend/service/anamnese/AnamneseService.java
@@ -1,11 +1,28 @@
 package com.lumina.backend.service.anamnese;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lumina.backend.dto.anamnese.AnamneseMapper;
+import com.lumina.backend.dto.anamnese.OcrRespostaDTO;
+import com.lumina.backend.exception.AnamneseVazio;
 import com.lumina.backend.exception.EntidadeNaoEncontrada;
 import com.lumina.backend.model.Anamnese;
+import com.lumina.backend.model.Cliente;
 import com.lumina.backend.repository.AnamneseRepository;
+import com.lumina.backend.service.openIA.GeminiAIService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.services.textract.TextractClient;
+import software.amazon.awssdk.services.textract.model.BlockType;
+import software.amazon.awssdk.services.textract.model.DetectDocumentTextRequest;
+import software.amazon.awssdk.services.textract.model.DetectDocumentTextResponse;
+import software.amazon.awssdk.services.textract.model.Document;
+import lombok.extern.slf4j.Slf4j;
 
-import java.util.List;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.stream.Collectors;
 
 @Service
 public class AnamneseService {
